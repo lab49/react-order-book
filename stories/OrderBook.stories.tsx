b78d39f97782dd09be2a32320ea28ca2cf963551
Brian McAllister <brian@brianmcallister.com>
2020-08-10 18:19:55 -0400

    Add another styling example.

--- stories/OrderBook.stories.tsx
+++ stories/OrderBook.stories.tsx
@@ -251,3 +251,105 @@ export const MakeItNice = () => {
     </>
   );
 };
+
+export const MakeItNiceAgain = () => {
+  return (
+    <>
+      <style
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+            .MakeItNiceAgain {
+              background-color: #151825;
+              color: rgba(255, 255, 255, 0.6);
+              display: inline-block;
+              font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+              font-size: 13px;
+              font-variant-numeric: tabular-nums;
+              padding: 50px 0;
+            }
+
+            .MakeItNiceAgain__side-header {
+              font-weight: 700;
+              margin: 0 0 5px 0;
+              text-align: right;
+            }
+
+            .MakeItNiceAgain__list {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+            }
+
+            .MakeItNiceAgain__list-item {
+              border-top: 1px solid rgba(255, 255, 255, 0.1);
+              cursor: pointer;
+              display: flex;
+              justify-content: flex-end;
+            }
+
+            .MakeItNiceAgain__list-item:before {
+              content: '';
+              flex: 1 1;
+              padding: 3px 5px;
+            }
+
+            .MakeItNiceAgain__side--bids .MakeItNiceAgain__list-item {
+              flex-direction: row-reverse;
+            }
+
+            .MakeItNiceAgain__side--bids .MakeItNiceAgain__list-item:last-child {
+              border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+            }
+
+            .MakeItNiceAgain__side--bids .MakeItNiceAgain__size {
+              text-align: right;
+            }
+
+            .MakeItNiceAgain__list-item:hover {
+              background: #262935;
+            }
+
+            .MakeItNiceAgain__price {
+              border-left: 1px solid rgba(255, 255, 255, 0.1);
+              border-right: 1px solid rgba(255, 255, 255, 0.1);
+              color: #b7bdc1;
+              display: inline-block;
+              flex: 0 0 50px;
+              margin: 0;
+              padding: 3px 5px;
+              text-align: center;
+            }
+
+            .MakeItNiceAgain__size {
+              flex: 1 1;
+              margin: 0;
+              padding: 3px 5px;
+              position: relative;
+            }
+
+            .MakeItNiceAgain__size:before {
+              background-color: var(--row-color);
+              content: '';
+              height: 100%;
+              left: 0;
+              opacity: 0.08;
+              position: absolute;
+              top: 0;
+              width: 100%;
+            }
+          `,
+        }}
+      />
+
+      <OrderBook
+        book={{ bids: book.bids, asks: book.asks }}
+        fullOpacity
+        interpolateColor={(color) => color}
+        listLength={10}
+        stylePrefix="MakeItNiceAgain"
+        showSpread={false}
+      />
+    </>
+  );
+};
