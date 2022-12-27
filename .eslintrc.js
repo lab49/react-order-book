54df99bdd29a99c8705f7ab109d040f57d2ce970
Brian McAllister <brian.mcallister@lab49.com>
2022-12-27 13:54:24 -0500

    Upgrade Storybook.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,22 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.eslint.json',
+    project: './tsconfig.eslint.json'
   },
   env: {
-    jest: true,
+    jest: true
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'airbnb-typescript',
-    'plugin:eslint-comments/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ['airbnb-typescript', 'plugin:eslint-comments/recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier', 'plugin:storybook/recommended'],
   rules: {
     'prettier/prettier': 2,
     'import/prefer-default-export': 0,
-    'import/no-default-export': 2,
+    'import/no-default-export': 2
   },
-  overrides: [
-    {
-      files: ['./stories/**/*stories*'],
-      rules: {
-        'import/no-default-export': 0,
-      },
-    },
-  ],
+  overrides: [{
+    files: ['./stories/**/*stories*'],
+    rules: {
+      'import/no-default-export': 0
+    }
+  }]
 };
